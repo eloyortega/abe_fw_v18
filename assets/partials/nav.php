3b4d386950c0b5057233ab7cd0398ed3eca149a1
eloy ortega <info@artbyeloi.com>
2018-11-20 16:59:26 -0500

    added common js functionalilty

---
+++ assets/partials/nav.php
@@ -0,0 +1,24 @@
+	<!-- Navbar -->
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<!-- <nav class="navbar navbar-expand-lg navbar-dark bg-dark"> -->
+		<a class="navbar-brand" href="#">ABE Template 2018</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+			<ul class="navbar-nav">
+				<li class="nav-item dropdown active">
+					<a class="nav-link" href="index.html">Home</a>
+				</li>
+				<li class="nav-item dropdown">
+					<a class="nav-link" href="#">CSS Components</a>
+				</li>
+				<li class="nav-item dropdown">
+					<a class="nav-link" href="#">JS Components</a>
+				</li>
+				<li class="nav-item dropdown">
+					<a class="nav-link" href="#">PHP Components</a>
+				</li>
+			</ul>
+		</div>
+	</nav>
