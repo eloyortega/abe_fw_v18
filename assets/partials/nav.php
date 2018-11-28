0df9f4a4454e5d49ca8c247731d89705ff258856
eloy ortega <info@artbyeloi.com>
2018-11-28 17:18:10 -0500

    php blog

--- assets/partials/nav.php
+++ assets/partials/nav.php
@@ -7,12 +7,46 @@
 		</button>
 		<div class="collapse navbar-collapse justify-content-end" id="navbarNav">
 			<ul class="navbar-nav">
-				<li class="nav-item dropdown active">
-					<a class="nav-link" href="appbloglist.php">Home</a>
+				<li class="nav-item">
+					<a class="dropdown-item" href="index.html">Home</a>
+				</li>
+				<li class="nav-item">
+					<a class="dropdown-item" href="formresponse.html">Ajax Form</a>
+				</li>
+				<li class="nav-item">
+					<a class="dropdown-item" href="appdynamichtml.php">Dynamic HTML</a>
+				</li>
+				<li class="nav-item">
+					<a class="dropdown-item" href="appsearch.php">Search</a>
 				</li>
 				<li class="nav-item dropdown">
-					<a class="nav-link" href="appblogcreate.php">Create</a>
+					<a class="dropdown-item" href="appemail.php">Email</a>
 				</li>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">Authentication</a>
+					<div class="dropdown-menu">
+						<a class="dropdown-item" href="appreg.php">Registration</a>
+						<a class="dropdown-item" href="applogin.php">Login</a>
+						<a class="dropdown-item" href="applogout.php">Logout</a>
+						<a class="dropdown-item" href="appadmin.php">Admin</a>
+					</div>
+				</li>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">Blog</a>
+					<div class="dropdown-menu">
+						<a class="dropdown-item" href="appbloglist.php">Blog</a>
+						<a class="dropdown-item" href="appblogcreate.php">Create Article</a>
+					</div>
+				</li>
+						
+						
+						
+						
+						
+						
+						
+						
+						
 			</ul>
 		</div>
 	</nav>
